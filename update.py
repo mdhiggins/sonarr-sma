86b258b464bfcfc0561470a4319551f8fc6c05fb
Michael Higgins <mdhiggins23@gmail.com>
2020-02-17 13:05:03 -0500

    Update update.py
    pull more from sonarr config

--- update.py
+++ update.py
@@ -15,34 +15,40 @@ def main():
         print("No Sonarr/Radarr config file found")
         return
 
-    tree = ET.parse(xml)
-    root = tree.getroot()
-    port = root.find("Port").text
-    apikey = root.find("ApiKey").text
-    section = sections.get(port)
-
-    ip = os.environ.get("HOST")
-    ssl = os.environ.get("SSL")
-    webroot = os.environ.get("WEBROOT")
-
     if not os.path.isfile(autoProcess):
         print("autoProcess.ini does not exist")
         return
 
+    tree = ET.parse(xml)
+    root = tree.getroot()
+    port = root.find("Port").text
+    sslport = root.find("SslPort").text
+    webroot = root.find("UrlBase").text
+    webroot = webroot if webroot else ""
+    ssl = root.find("EnableSsl").text
+    ssl = ssl.lower() in ["true", "yes", "t", "1", "y"] if ssl else False
+    apikey = root.find("ApiKey").text
+    section = sections.get(port)
+
     safeConfigParser = configparser.SafeConfigParser()
     safeConfigParser.read(autoProcess)
-    safeConfigParser.set(section, "apikey", apikey)
-    if ip:
-        safeConfigParser.set(section, "host", ip)
-    if ssl:
-        safeConfigParser.set(section, "SSL", ssl)
-    if webroot:
-        safeConfigParser.set(section, "web_root", webroot)
-    if port:
-        safeConfigParser.set(section, "port", port)
 
+    # Set FFMPEG/FFProbe Paths
     safeConfigParser.set("MP4", "ffmpeg", "/usr/local/bin/ffmpeg")
     safeConfigParser.set("MP4", "ffprobe", "/usr/local/bin/ffprobe")
+
+    # Set values from config.xml
+    safeConfigParser.set(section, "apikey", apikey)
+    safeConfigParser.set(section, "SSL", str(ssl))
+    safeConfigParser.set(section, "port", sslport if ssl else port)
+    safeConfigParser.set(section, "web_root", webroot)
+
+    # Set IP from environment variable
+    ip = os.environ.get("HOST")
+    if ip:
+        safeConfigParser.set(section, "host", ip)
+    else:
+        safeConfigParser.set(section, "host", "127.0.0.1")
 
     fp = open(autoProcess, "w")
     safeConfigParser.write(fp)
